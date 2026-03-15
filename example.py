3c7e4e3e920ad6f7fd7bbb830900b14c5809d2db
Mohammad Al Amin <mohammadalamin0075@gmail.com>
2026-03-15 15:56:41 +0600

    added code and gitignore file

--- example.py
+++ example.py
@@ -44,3 +44,32 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+@app.post("/heroes/")
+def create_heroe(hero: Hero, session: SessionDep) -> Hero:
+    session.add(hero)
+    session.commit( )
+    session.refresh(hero)
+    return hero
+
+
+@app.get("/heros/")
+def read_heroes(session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=100)]=100,) -> list[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit).all())
+    return heroes
+
+@app.get("/heroes/{hero_id}")
+def read_hero(session: SessionDep, hero_id: int) -> Hero:
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+@app.delete("heroes/{hero_id}")
+def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
+
